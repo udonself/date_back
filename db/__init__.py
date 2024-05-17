8d7943c6abf4c39d9b7bccf179902a6f960be6a6
udonself <121800517+udonself@users.noreply.github.com>
2024-05-17 20:39:55 +0300

    default

--- db/__init__.py
+++ db/__init__.py
@@ -1,3 +1,3 @@
-from .models import User, Photo, Like, Dislike, Match, Message
+from .models import User, Like, Dislike, Match, Message
 from .pydantic_models import UserRegister, UserUpdate
 from .database import get_db, depends_db
